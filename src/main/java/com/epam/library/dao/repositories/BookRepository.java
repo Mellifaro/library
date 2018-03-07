2a2c1343790f3c658acd693607af8215ce1d532c
Viktor Skapoushchenko <viktor_skapoushchenko@epam.com>
2018-03-07 19:03:46 +0200

    Created repositories for books and authors

---
+++ src/main/java/com/epam/library/dao/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package com.epam.library.dao.repositories;
+
+import com.epam.library.dao.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long>{
+}
